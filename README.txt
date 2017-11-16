e2882c93dd5276278dcdc833a5b3f0d7da513a6b
NapasinKuna <napasin.644@gmail.com>
2017-11-17 02:25:45 +0700

    First commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+This project is Verily Vision Automatic License Plate Recognition system
+engine create for Commercial use version,0.1. 
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='VALPR',
+    version='0.1dev',
+    packages=['vlpr',],
+    license='Verily Vision co.,ltd. for testing VALPR system license',
+    long_description=open('README.txt').read(),
+)
